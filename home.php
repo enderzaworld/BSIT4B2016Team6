bd5f64b1325087e2ed21c6d6cabad6ade11179d1
Christian Zorrilla <CLSZorrilla@gmail.com>
2017-02-06 05:29:52 +0800

    06/02/2017

---
+++ home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>TUP - Manila</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="bootstrap-3.3.7/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+<div id="header">
+	<div class="background">
+		<div id="logo">
+			<a href="index.html"><img src="images/tup-logo.png" alt="LOGO" height="110" width="110">&nbsp;Technological University of the Philippines</a>
+		</div>
+		<div id="navigation">
+			<ul>
+				<li class="selected">
+					<a href="home.php">Home</a>
+				</li>
+				<li>
+					<a href="users.php">Users</a>
+				</li>
+				<li>
+					<a href="documents.php">Documents</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div id="page">		
+	<div id="contents">
+			
+					
+			
+	</div>
+	
+	<div id="footer">
+		<div class="background">
+			<div class="body">				
+				<p id="footnote">
+					© Copyright 2017. All rights reserved.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
